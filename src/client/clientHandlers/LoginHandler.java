42ec1bc48d79855e50c9d9561754ef550f0fc73a
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2018-06-15 01:47:21 +0430

    client login operation handler

---
+++ src/client/clientHandlers/LoginHandler.java
@@ -0,0 +1,59 @@
+package client.clientHandlers;
+
+import client.ClientGui;
+import client.ClientStart;
+import client.LoginFrame;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class LoginHandler implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (((JButton) e.getSource()).getActionCommand()) {
+            case ClientGui.COMMAND_LOGIN:
+                connectAngLogin();
+                break;
+            case ClientGui.COMMAND_CANCEL:
+                LoginFrame.frame.setVisible(false);
+                break;
+            case ClientGui.COMMAND_CLEAR_ALL:
+                clearAllOperation();
+                break;
+        }
+    }
+
+
+    private void clearAllOperation() {
+        LoginFrame.usernameField.setText("");
+        LoginFrame.passwordField.setText("");
+    }
+
+    private void connectAngLogin() {
+        try {
+            String guiUsername = LoginFrame.usernameField.getText();
+            String guipass = LoginFrame.passwordField.getText();
+            Socket socket = new Socket(ClientStart.LOCAL, ClientStart.DEFAULT_PORT_LOG);
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            String toSend = "[USERNAME]" + guiUsername + "\n[PASS]" + guipass;
+            dataOutputStream.writeUTF(toSend);
+            dataOutputStream.flush();
+            int response = dataInputStream.readInt();
+            if (response == ClientStart.RESPONSE_GOOD) {
+                JOptionPane.showMessageDialog(null, "You logged in.\nThank you", "Login", JOptionPane.INFORMATION_MESSAGE);
+                ClientStart.isLoggedIn = true;
+            } else if (response == ClientStart.RESPONSE_BAD_NOUSER)
+                JOptionPane.showMessageDialog(null, "Error logging in.\nSorry.\nCheck your username and password or register.", "Error", JOptionPane.WARNING_MESSAGE);
+            else
+                JOptionPane.showMessageDialog(null, "Error in internet connection with server.\ncheck your connection or call us.", "Error", JOptionPane.WARNING_MESSAGE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
